62b3c71828f42d1de2cf8346f92a76cfe91e8768
George Zed <georlema@gmail.com>
2021-02-21 02:56:49 +0200

    project restructure

--- src/helpers/fetchPlayers.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = `http://localhost:3000/`;
-
-export default function fetchPlayers(setPlayers) {
-  fetch(`${BASE_URL}players`)
-    .then((res) => res.json())
-    .then((data) => setPlayers(data));
-}
